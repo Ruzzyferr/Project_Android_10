d847d5ac4ef3494168061f1cac7e64fb4091d64d
Ruzzyferr <bulutruzgaremir@gmail.com>
2023-01-08 03:04:14 +0100

    Files saved succesfully ! next step: inverted text

--- app/src/main/java/com/example/project_ruzgar_bulut/MainActivity.java
+++ app/src/main/java/com/example/project_ruzgar_bulut/MainActivity.java
@@ -4,35 +4,29 @@ import static android.os.Environment.getExternalStoragePublicDirectory;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.SharedPreferencesCompat;
 
-import android.app.LauncherActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.Switch;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
@@ -66,50 +60,81 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
-        file = new File(getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),"project.pdf");
-
-
         listView = (ListView) findViewById(R.id.liste);
         editText1 = (EditText) findViewById(R.id.editText1);
         editText2 = (EditText) findViewById(R.id.editText2);
         btnAdd = (Button) findViewById(R.id.btnEntry);
         btnSave = findViewById(R.id.btnSave);
 
-        ArrayList<texts> arrayList = new ArrayList<>();
 
+        ArrayList<texts> arrayList = new ArrayList<>();
+        HashMap<String, String> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
 
 
         aSwitch = findViewById(R.id.darkModeSwitch);
         // i used shared preferences to save the mode if you exit and open again
-        sharedPreferences = getSharedPreferences("MODE",Context.MODE_PRIVATE);
+        sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE);
 
-        nightMode = sharedPreferences.getBoolean("night",false); //light mode is default
+        nightMode = sharedPreferences.getBoolean("night", false); //light mode is default
+        adapter = new textAdapter(this, R.layout.list_row, arrayList);
 
-        if(nightMode){
-            aSwitch.setChecked(true);
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        //receiving saved list
+        Set<String> setRetrieve = sharedPreferences.getStringSet("map", new HashSet<String>());
+        for (String s : setRetrieve) {
+            String[] parts = s.split(":");
+            map.put(parts[0], parts[1]);
         }
 
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            arrayList.add(new texts(entry.getKey(), entry.getValue()));
+        }
+        listView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+        if (nightMode) {
+            aSwitch.setChecked(true);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+        }
+
+
+        //night mode - light mode
         aSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (nightMode){
+
+                // list view was reseting itself when i turn on - off darkmode. I wrote this
+                //to fix this problem
+                Set<String> setRetrieve = sharedPreferences.getStringSet("map", new HashSet<String>());
+                for (String s : setRetrieve) {
+                    String[] parts = s.split(":");
+                    map.put(parts[0], parts[1]);
+                }
+
+                for (Map.Entry<String, String> entry : map.entrySet()) {
+                    arrayList.add(new texts(entry.getKey(), entry.getValue()));
+                }
+                listView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
+
+                //darkmode control
+                if (nightMode) {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                     editor = sharedPreferences.edit();
-                    editor.putBoolean("night",false);
-                }else{
+                    editor.putBoolean("night", false);
+                } else {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                     editor = sharedPreferences.edit();
-                    editor.putBoolean("night",true);
+                    editor.putBoolean("night", true);
                 }
-            editor.apply();
+
+                editor.apply();
+
             }
 
         });
 
-
-
-        adapter = new textAdapter(this,R.layout.list_row, arrayList);
 
         //add entries
         btnAdd.setOnClickListener(new View.OnClickListener() {
@@ -119,31 +144,40 @@ public class MainActivity extends AppCompatActivity {
                 String subtext = editText2.getText().toString();
 
                 arrayList.add(new texts(text, subtext));
+                map.put(text, subtext);
 
+                for (Map.Entry<String, String> entry : map.entrySet()) {
+                    set.add(entry.getKey() + ":" + entry.getValue());
+                }
+                editor = sharedPreferences.edit();
+                editor.putStringSet("map", set);
 
                 listView.setAdapter(adapter);
                 adapter.notifyDataSetChanged();
-
+                editor.apply();
 
             }
 
         });
 
+
         //save Data
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    FileOutputStream fos = new FileOutputStream(file);
-                    ObjectOutputStream oos = new ObjectOutputStream(fos);
-                    oos.writeObject(arrayList);
-                    oos.close();
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                // Assume that the set of strings is called "stringSet"
 
-                Toast.makeText(MainActivity.this, "Saved Succesfully: Downloads", Toast.LENGTH_SHORT).show();
+                File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "strings.txt");
+                try (FileOutputStream outputStream = new FileOutputStream(file)) {
+                    for (String s : set) {
+                        outputStream.write(s.getBytes());
+                        outputStream.write("\n".getBytes());  // Write a newline character after each string
+                    }
+                    Toast.makeText(getApplicationContext(), "File saved successfully!",
+                            Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    // Handle the exception
+                }
             }
         });
 
@@ -156,8 +190,6 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, o.getText1().toString(), Toast.LENGTH_SHORT).show();
             }
         });
-
-
 
 
     }
